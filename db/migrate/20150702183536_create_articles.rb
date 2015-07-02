6150615ab9bf55dafc687f3e9b5bd692e0716dd9
Keith <keithlang@yahoo.com>
2015-07-02 15:04:51 -0500

    new articles

---
+++ db/migrate/20150702183536_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :party, limit: 2
+      t.integer :category_id
+      t.timestamps null: false
+    end
+  end
+end
