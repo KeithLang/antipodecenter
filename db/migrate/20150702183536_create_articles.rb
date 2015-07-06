a8b771d07f9d85a15ccbb4441d1f140f9bf0cc70
Keith <keithlang@yahoo.com>
2015-07-06 13:23:22 -0500

    first draft

--- db/migrate/20150702183536_create_articles.rb
+++ db/migrate/20150702183536_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title, null: false
       t.string :url, null: false
-      t.string :party, limit: 2
+      t.string :party, limit: 10
       t.integer :category_id
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702183536) do
+ActiveRecord::Schema.define(version: 20150706171911) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       limit: 255, null: false
     t.string   "url",         limit: 255, null: false
-    t.string   "party",       limit: 2
+    t.string   "party",       limit: 10
     t.integer  "category_id", limit: 4
     t.datetime "created_at",              null: false
     t.datetime "updated_at",              null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
