a86ac5b77781ac41dcbebabfb934631df74332e8
Keith <keithlang@yahoo.com>
2015-07-20 21:14:52 +0100

    Helpers Party, Category

--- app/helpers/select_helper.rb
+++ app/helpers/select_helper.rb
@@ -2,4 +2,11 @@ module SelectHelper
   def party_select
     [['Democrats', 'democrats'], ['Republicans', 'republicans']]
   end
+
+  def category_select
+    Category.all.map do |c|
+      [c.name, c.id]
+    end  
+  end
+
 end
