9c90aaf4465bd83bc5e3869109d3ac3cdafac9c7
Keith <keithlang@yahoo.com>
2015-07-25 14:39:15 +0200

    last changes

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -1,8 +1,26 @@
 class Admin::ArticlesController < Admin::DashboardController
   UPDATE_PARAMS = [:title, :url, :party, :category_id]
+  CREATE_PARAMS = [:title, :url, :party, :category_id]
   def index
   	@articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+    local_params = params.require(:article).permit(CREATE_PARAMS)
+    @article.attributes = local_params
+    if @article.save
+      flash[:notice] = 'Article created'
+      redirect_to admin_articles_path
+    else
+      flash[:error] = 'Something was wrong'
+      render :new
+    end
+  end  
 
   def edit
   	@article = Article.find(params[:id])
