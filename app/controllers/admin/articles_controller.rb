fb66cd27407b4238822d82031dca6eaae2d4f2b0
Keith <keithlang@yahoo.com>
2015-07-25 16:20:36 +0200

    Added edit/new category & delete article functionality

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -37,6 +37,13 @@ class Admin::ArticlesController < Admin::DashboardController
       flash[:error] = 'Something was wrong'
       render :edit
     end
+  end
+
+  def destroy
+    @article= Article.find(params[:id])
+    @article.destroy
+
+    redirect_to admin_articles_path
   end  
 
 end
--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,43 @@
 class Admin::CategoriesController < Admin::DashboardController
+  UPDATE_PARAMS = [:name]
+  CREATE_PARAMS = [:name]  
   def index
   	@categories = Category.all
   end
 
-  def edit
-  	@category = Category.find(params[:id])
+  def new
+    @category = Category.new
   end
+  
+
+  def create
+    @category = Category.new
+    local_params = params.require(:category).permit(CREATE_PARAMS)
+    @category.attributes = local_params
+    if @category.save
+      flash[:notice] = 'Category created'
+      redirect_to admin_categories_path
+    else
+      flash[:error] = 'Something was wrong'
+      render :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    local_params = params.require(:category).permit(UPDATE_PARAMS)
+    @category.attributes = local_params
+    if @article.save
+      flash[:notice] = 'Category saved'
+      redirect_to admin_categories_path
+    else
+      flash[:error] = 'Something was wrong'
+      render :edit
+    end
+  end  
+
 end
